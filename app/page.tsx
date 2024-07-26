21437f507b6e058e44eec16b0394c5e20ceb1c3e
leomosley <leo.mosley2@gmail.com>
2024-07-27 00:33:45 +0100

    fix(example link) update url

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,20 @@ import { SiTypescript } from "react-icons/si";
 
 export default function Home() {
   const technologies = [
-    { 
+    {
       name: "Next.js", icon: <RiNextjsFill className="w-10 h-10" />, href: "https://nextjs.org/",
-      description: `Next.js offers a heavily optimised approach to creating applications using React.` 
+      description: `Next.js offers a heavily optimised approach to creating applications using React.`
     },
-    { 
+    {
       name: "TypeScript", icon: <SiTypescript className="w-10 h-10 text-blue-500 bg-white rounded" />, href: "https://www.typescriptlang.org/",
-      description: `TypeScript will help you be a better developer. The "strictness" of TypeScript leads to smoother building.` 
+      description: `TypeScript will help you be a better developer. The "strictness" of TypeScript leads to smoother building.`
     },
-    { 
+    {
       name: "Tailwind", icon: <RiTailwindCssFill className="w-10 h-10" />, href: "https://tailwindcss.com/",
       description: `Tailwind is a utility-first CSS framework that helps you build beautiful, responsive designs without any extra configuration.`
     },
   ];
-  
+
   return (
     <main className="flex flex-col justify-center mx-auto md:max-w-6xl">
       <Header />
@@ -38,7 +38,7 @@ export default function Home() {
             "rounded-full md:text-lg font-semibold text-neutral-950",
             "bg-gray-200/90 hover:bg-gray-200",
           )}
-          href="https://mosly.dev"
+          href="https://example.boring-portfolio.dev"
           target="_blank"
         >
           Example Site <HiExternalLink className="w-5 h-5" />
@@ -61,10 +61,10 @@ export default function Home() {
           <h2 className="tracking-tight font-extrabold text-2xl sm:text-3xl md:text-4xl">Getting Started</h2>
           <p className="md:text-lg">
             I made create-boring-portfolio to help people like me who dont want the fuss of desiging and building a portfolio.
-            All you have to do is run one command and it builds you a production ready portfolio website. 
+            All you have to do is run one command and it builds you a production ready portfolio website.
           </p>
           <p className="md:text-lg">
-            The design is super simple, it is heavily infulenced by&nbsp; 
+            The design is super simple, it is heavily infulenced by&nbsp;
             <a className="font-semibold underline-offset-4 underline decoration-neutral-500 transition hover:decoration-inherit" href="https://www.nexxel.dev/" target="blank">nexxel.dev</a>
             . I think its the perfect design.
           </p>
@@ -81,7 +81,7 @@ export default function Home() {
         <div className="flex flex-col sm:flex-row gap-6 mt-4 mb-8 px-4 sm:px-0">
           {technologies.map((item, index) => (
             <a
-              key={index} 
+              key={index}
               className={clsx(
                 "flex flex-col flex-1 rounded-lg transition group",
                 "bg-neutral-900/60 border border-neutral-800 backdrop-blur-sm",
