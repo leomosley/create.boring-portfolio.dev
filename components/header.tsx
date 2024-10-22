1fd11a3cca2f27c0a7203eeffa3687e330adb760
leomosley <leo.mosley2@gmail.com>
2024-10-22 17:16:23 +0100

    feat(header): git stars

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
+import clsx from 'clsx';
 import Image from 'next/image';
-import React from 'react';
 import { FaGithub, FaStar } from 'react-icons/fa6';
 
 async function getStars() {
@@ -9,7 +9,7 @@ async function getStars() {
       throw new Error('Failed to fetch repos');
     }
     const data = await res.json();
-    return data.stargazers_count;
+    return data.stargazers_count as number;
 
   } catch (error) {
     console.error(error);
@@ -17,7 +17,8 @@ async function getStars() {
 }
 
 export default async function Header() {
-  // const stars = await getStars();
+  const stars = await getStars();
+
   return (
     <header className="flex items-center p-4 w-full">
       <a
@@ -34,10 +35,20 @@ export default async function Header() {
         />
       </a>
       <a
-        className="flex gap-3 items-center ml-auto text-gray-200"
+        className="group flex gap-3 items-center ml-auto text-gray-200"
         href="https://github.com/leomosley/create-boring-portfolio"
         target="_blank"
       >
+        {stars && (
+          <div className={clsx(
+            "relative items-center rounded-lg border bg-clip-padding px-2 py-1 no-underline !transition-colors !duration-300 flex ltr:mr-[.33em] rtl:ml-[.33em]",
+            "border-neutral-300/20 bg-neutral-300/10 text-slate-100 group-hover:border-neutral-300/50 group-hover:bg-neutral-300/10",
+            "after:absolute after:h-0 after:w-0 after:border-8 after:border-transparent after:transition-colors after:duration-300 after:right-[calc(-0.5em-1px)] after:border-r-0 after:border-l-neutral-300/20 after:group-hover:border-l-neutral-300/50")
+          }>
+            <FaStar className="w-4 h-4 mr-1" />
+            {stars}
+          </div>
+        )}
         <FaGithub className="w-10 h-10" />
       </a>
     </header>
